9a11c5dea991c387dda23c792972b940029501d8
Aldo Paniagua <aldo.paniagua@hotmail.com>
2024-10-25 14:58:22 -0400

    First commit

---
+++ src/App-test.js
@@ -0,0 +1,31 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  function formatName(userName) {
+    return userName.toUpperCase();
+  }
+
+  function getGreeting(userName) {
+    if (userName) {
+      return <h1>Hello {formatName(userName)}!</h1>;
+    } else {
+      return <h1>Hello Stranger</h1>;
+    }
+  }
+
+  const name = 'John Smith';
+  const userImage = '/logo192.png';
+  const image = <img src={userImage} alt="logo" />;
+  const element = (
+    <div>
+      <h1>Hello {name}</h1>
+      <p>Good to see you</p>
+      {image}
+    </div>
+  );
+
+  return element;
+}
+
+export default App;
--- src/index-test2.js
+++ src/index-test2.js
@@ -9,7 +9,9 @@ import Message from './Message';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    {/* <App /> */}
+    <Welcome name="John" />
+    <Message message="React is great!" />
   </React.StrictMode>
 );
 
