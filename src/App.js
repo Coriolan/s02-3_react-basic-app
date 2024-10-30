94c81e025d838488b82ffa895d31a6a4d13273e8
Aldo Paniagua <Aldo.AntonioPaniaguaHerrera@cbsa-asfc.gc.ca>
2024-10-30 11:19:04 -0400

    Create conditionally rendered components

--- src/App.js
+++ src/App.js
@@ -3,14 +3,18 @@ import Welcome from './Welcome';
 import Message from './Message';
 import Clock from './Clock';
 import ToggleButton from './ToggleButton';
+import Greeting from './Greeting';
+import LoginControl from './LoginControl';
 
 function App() {
   return (
     <div>
-      <Welcome name="John" />
+      {/* <Welcome name="John" />
       <Message message="React is great!" />
       <Clock />
       <ToggleButton />
+      <Greeting isLoggedIn /> */}
+      <LoginControl/>
     </div>
   );
 }
